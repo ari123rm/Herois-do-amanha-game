f4da2d3ee3c557f2025676849f7a3a2fbb84bfcc
ari123rm <arielmoraes05@gmail.com>
2024-03-17 09:14:08 -0300

    tentativa GITHUB

--- js/jogo.js
+++ js/jogo.js
@@ -95,7 +95,7 @@ function abrindo_inventario (){
             sprites.style.display = "none";
             falaAberto = true;
         }
-        jogo.style.backgroundImage = "url(./imagens/fichaHerois.png)";
+        jogo.style.backgroundImage = "url('./imagens/FichaHerois.png')";
         criarFicha();
         salvarFala = texto_fala.innerText;
         salvarEscolhas = [escolha_1.innerText,escolha_2.innerText,escolha_3.innerText];
